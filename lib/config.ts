aa35de34075e17e31d940f73600ebe3b6efc5735
amoscookeh <amosliew1999@gmail.com>
2026-01-04 00:34:01 +0800

    style: format config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@
 // process.env[key] doesn't work - must use literal property access
 const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000";
 
-console.log('[Config] API URL:', apiUrl);
+console.log("[Config] API URL:", apiUrl);
 
 export const config = {
   apiUrl,
